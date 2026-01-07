e140e4f9d50bcd50fc90416c9baf69d0dd372769
isapaula <is214311@gmail.com>
2026-01-06 21:25:10 -0300

    Arquivos da model

---
+++ App/Models/Professor.php
@@ -0,0 +1,7 @@
+<?php 
+
+namespace Models; 
+
+class Professor extends Usuario {
+
+} 
---
+++ App/Models/Usuario.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Models; 
+
+abstract class Usuario {
+
+    protected int $id; 
+    protected string $nome;
+    protected string $email; 
+    protected string $senha;
+
+}
