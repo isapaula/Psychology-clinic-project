70212910a06ebfb439da7986871af83ea5e22fa9
isapaula <is214311@gmail.com>
2026-02-24 22:53:43 -0300

    Tela de login funcionando!

--- App/Controller/UsuarioController.php
+++ App/Controller/UsuarioController.php
@@ -2,19 +2,56 @@
 
 namespace Controller;
 
-use Models\Usuario;
+use Database\Conexao;
 
 class UsuarioController {
 
-
     public function login(){
+
+        require dirname(__DIR__, 2 ). '/App/Views/Login.php';
+    }
+
+    public function store(){
+
+        try {
+
+        $pdo = Conexao::getConnection();
+        
+        $login = isset($_POST['user']) ? $_POST['user'] : "Usuário/e-mail inválido!";
+        $pass = isset($_POST['pass']) ? $_POST['pass'] : "Senha inválida!";
+        $senhaHash = password_hash($pass, PASSWORD_DEFAULT);
+
+        $smtp = $pdo->prepare("SELECT usuario.email_user, usuario.senha_user  FROM usuario WHERE email_user = ? OR senha_user = ?");
+
+        $smtp->execute([$login, $senhaHash]); 
+
+        $row = $smtp->fetchAll(\PDO::FETCH_ASSOC); 
+
+        if (count($row) > 0) {
+            $_SESSION['usuario'] = $login;
+            $_SESSION['senha'] = $senhaHash;
+
+            echo "usuario logado";
+        }else if(count($row) == 0){
+            echo "Não foi possível logar usuário!";
+        }
+            
+        } catch (\Exception $e) {
+
+            echo "Erro ao logar:  ". $e->getMessage();
+            
+        }
 
     }
 
     public function deslogar(){
 
     }
-    
+
+    public function selecaoPerfil(){
+
+    }
+
 
 
 }                                                                                 
--- App/Views/Login.php
+++ App/Views/Login.php
@@ -7,9 +7,15 @@
 </head>
 <body>
 
-<form action="" method="post">
+<h1>Tela de Login</h1>
 
+<form action="/Psychology-clinic-project/public/usuario/store" method="post">
 
+    <input type="text" name="user" id="user" placeholder="Digite seu e-mail" required>
+    <br><br>
+    <input type="password" name="pass" id="pass" placeholder="*********" required>
+    <br><br>
+    <button type="submit">Logar</button>
 </form>
 </body>
 </html>
